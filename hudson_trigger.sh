ba28813f54229c267db97cf1d97512bb9a7ec0e8
tsaikd <cheng-feng.tsai@foxconn.com>
2010-12-29 11:36:10 +0800

    hudson_trigger.sh: add option for building branch

--- hudson_trigger.sh
+++ hudson_trigger.sh
@@ -13,7 +13,8 @@ function usage() {
 Usage: ${PN} [Options] <Project Name> <Token>
 
 Options:
-  -h        : Show this help message
+  -h          : Show this help message
+  -b <BRANCH> : Set branch name
 
 Current Config:
   hudson_url: '${hudson_url}'
@@ -23,14 +24,15 @@ EOF
 
 type getopt cat wget >/dev/null || exit $?
 
-(($# != 2)) && usage "Invalid parameters"
-opt="$(getopt -o h -- "$@")"
+(($# < 2)) && usage "Invalid parameters"
+opt="$(getopt -o hb: -- "$@")"
 (($? != 0)) && usage "Parse options failed"
 
 eval set -- "${opt}"
 while true ; do
 	case "${1}" in
 	-h) usage ; shift ;;
+	-b) proj_branch="${2}" ; shift 2 ;;
 	--) shift ; break ;;
 	*) echo "Internal error!" ; exit 1 ;;
 	esac
@@ -39,13 +41,25 @@ done
 proj_name="${1}" ; shift
 token="${1}" ; shift
 
+if [ "${proj_branch}" ] ; then
+	branch="$(git rev-parse "${proj_branch}")"
+	opt_rev="&GIT_BUILD_BRANCH=${branch}"
+	info_rev=" branch (${branch})"
+	build_path="/buildWithParameters"
+else
+	branch=""
+	opt_rev=""
+	info_rev=""
+	build_path="/build"
+fi
+
 wget -q -O /dev/null \
-	"${hudson_url}/job/${proj_name}/build" \
-	--post-data="token=${token}"
+	"${hudson_url}/job/${proj_name}/${build_path}" \
+	--post-data="token=${token}${opt_rev}"
 
 page="$(wget -q -O - "${hudson_url}/job/${proj_name}/")"
 num="$(grep -Eo -m 1 "#[0-9]+" <<<"${page}")"
 if [ "${num}" ] ; then
-	echo "trigger hudson project (${proj_name}) build (${num})"
+	echo "trigger hudson project (${proj_name}) build (${num})${info_rev}"
 fi
 
